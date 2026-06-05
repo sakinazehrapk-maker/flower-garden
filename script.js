d9d89c797ea0a4637963f2b1c6974871c6f9d305
sakina <sakinazehrapk@gmail.com>
2026-06-06 02:11:10 +0500

    added flower growth and watering system

--- script.js
+++ script.js
@@ -6,4 +6,33 @@ plantBtn.addEventListener("click", () => {
     flower.classList.add("flower");
     flower.textContent = "🌱";
     garden.appendChild(flower);
+    setTimeout(() => {
+        flower.textContent = "🌿";
+    }, 3000);
+    setTimeout(() => {
+        flower.textContent = "🌷";
+    }, 6000);
+});
+const flower = document.createElement("div");
+flower.textContent = "🌱";
+flower.dataset.stage = "seed";
+
+ 
+const flowers = [];
+plantBtn.addEventListener("click", () => {
+    const flower = document.createElement("div");
+    flower.classList.add("flower");
+    flower.textContent = "🌱";
+    garden.appendChild(flower);
+    flowers.push(flower);
+});
+waterBtn.addEventListener("click", () => {
+    flowers.forEach(flower => {
+        if(flower.textContent === "🌱"){
+            flower.textContent = "🌿";
+        }
+        else if(flower.textContent === "🌿"){
+            flower.textContent = "🌷";
+        }
+    });
 });
